c2ad4926225d32f50b806a11e22ba04520e6b4f5
nikitawalunjkar031 <155815879+nikitawalunjkar031@users.noreply.github.com>
2024-02-15 14:30:00 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -2,13 +2,21 @@ async function fetchWeatherData(cityName){
 	let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=6e1344f4e0fdbd2d24ac8711cfe16c40`
 	const response = await fetch(url)
 	const data = await response.json();
-	console.log(data)
+	let city = document.getElementById("city");
+	city.innerHTML = data.name;
+	let windspeed = document.getElementById("wind_speed");
+	windspeed.innerHTML = data.wind.speed + "km/hr";
+	let humidity = document.getElementById("humidity");
+	humidity.innerHTML = data.main.humidity+ "g/kg";
+	let temp = document.getElementById("temp");
+	temp.innerHTML = Math.round((data.main.temp)-273.15)+"°C";
+	console.log(data);
 }
-//demo()
+
 
 function fetchCity(){
-	let cityName = document.getElementById("city_name");
-	if(cityName===""){
+	let cityName = document.getElementById("cityname");
+	if(cityName.value===""){
 		alert("Enter a city name");
 	}else{
 		fetchWeatherData(cityName.value);
